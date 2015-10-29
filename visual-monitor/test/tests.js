c64ee54d5ac3115ca8482f3a618889739913b5a2
David Kohav <david@gizra.com>
2015-10-29 14:14:14 +0200

    Add visual monitor.

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -63,10 +63,16 @@ describe('Visual monitor testing', function() {
       .url(baseUrl)
       .webdrivercss(testName + '.homepage', {
         name: '1',
-        exclude: [],
+        exclude:
+          [
+            // Carousel.
+            '.inside',
+            // Article.
+            '.items_group img'
+          ],
         remove: [],
         hide: [],
-        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+        screenWidth: selectedCaps == 'chrome' ? [960] : undefined,
       }, resultsCallback)
       .call(done);
   });
